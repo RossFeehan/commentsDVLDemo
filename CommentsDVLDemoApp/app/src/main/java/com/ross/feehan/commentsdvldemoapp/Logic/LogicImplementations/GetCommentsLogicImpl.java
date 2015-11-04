187c01970150accc3af5952698059329c445e4b7
rossfeehan <thafirstboss@yahoo.co.uk>
2015-11-04 12:19:14 +0000

    Created the impl class for GetComments Logic module and GetComments Data module that will implement the required module interface classes

---
+++ CommentsDVLDemoApp/app/src/main/java/com/ross/feehan/commentsdvldemoapp/Logic/LogicImplementations/GetCommentsLogicImpl.java
@@ -0,0 +1,8 @@
+package com.ross.feehan.commentsdvldemoapp.Logic.LogicImplementations;
+
+/**
+ * Created by Ross Feehan on 04/11/2015.
+ * Copyright Ross Feehan
+ */
+public class GetCommentsLogicImpl {
+}
