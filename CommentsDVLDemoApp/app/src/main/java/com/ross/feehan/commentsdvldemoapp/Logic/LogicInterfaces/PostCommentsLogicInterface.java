a2fbc91485b1b6277e64641b5624fd78f925b509
rossfeehan <thafirstboss@yahoo.co.uk>
2015-11-05 15:00:48 +0000

    Created post comments interface for the logic module

---
+++ CommentsDVLDemoApp/app/src/main/java/com/ross/feehan/commentsdvldemoapp/Logic/LogicInterfaces/PostCommentsLogicInterface.java
@@ -0,0 +1,16 @@
+package com.ross.feehan.commentsdvldemoapp.Logic.LogicInterfaces;
+
+import com.ross.feehan.commentsdvldemoapp.Data.Objects.Comment;
+
+/**
+ * Created by Ross Feehan on 05/11/2015.
+ * Copyright Ross Feehan
+ */
+public interface PostCommentsLogicInterface {
+
+    public void checkComment(Comment comment);
+
+    public void commentPostedSuccessfully();
+
+    public void commentUnsuccessfullyPosted();
+}
