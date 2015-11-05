a3aa6544cfbbb0fa1df259625dc191efba7d8706
rossfeehan <thafirstboss@yahoo.co.uk>
2015-11-05 14:56:32 +0000

    Created PostcommentInterface interface for the data module

---
+++ CommentsDVLDemoApp/app/src/main/java/com/ross/feehan/commentsdvldemoapp/Data/DataInterfaces/PostCommentInterface.java
@@ -0,0 +1,12 @@
+package com.ross.feehan.commentsdvldemoapp.Data.DataInterfaces;
+
+import com.ross.feehan.commentsdvldemoapp.Data.Objects.Comment;
+
+/**
+ * Created by Ross Feehan on 05/11/2015.
+ * Copyright Ross Feehan
+ */
+public interface PostCommentInterface {
+
+    public void postComment(Comment comment);
+}
